f03d968f65782d55d5defe90de1caba6c4e70d29
Igor Buzatovic <igor.buzatovic@inovatrend.com>
2019-11-11 14:02:05 +0100

    Consume messages from topic

--- src/main/java/rba_demo_1/SimpleConsumer.java
+++ src/main/java/rba_demo_1/SimpleConsumer.java
@@ -1,9 +1,14 @@
 package rba_demo_1;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.Properties;
 
 public class SimpleConsumer {
@@ -14,9 +19,19 @@ public class SimpleConsumer {
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-1");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
 
+        String topic = "rba-demo-1";
+        consumer.subscribe(Collections.singletonList(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.of(100, ChronoUnit.MILLIS));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("Consumed message, key: " + record.key() + ", value: " + record.value());
+            }
+        }
 
     }
 }

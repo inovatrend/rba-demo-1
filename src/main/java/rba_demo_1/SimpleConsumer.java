62d630412c8e50be7beae48a95a6c8879fc9d795
Igor Buzatovic <igor.buzatovic@inovatrend.com>
2019-11-11 12:37:57 +0100

    Added consumer instance with basic config

--- src/main/java/rba_demo_1/SimpleConsumer.java
+++ src/main/java/rba_demo_1/SimpleConsumer.java
@@ -1,8 +1,22 @@
 package rba_demo_1;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Properties;
+
 public class SimpleConsumer {
 
     public static void main(String[] args) {
+
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
+
 
     }
 }

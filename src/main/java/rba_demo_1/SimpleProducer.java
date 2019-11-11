b711b933cf71b4edfb987f7d3f575b12019cd8f3
Igor Buzatovic <igor.buzatovic@inovatrend.com>
2019-11-11 11:00:21 +0100

    Created producer instance with basic config

--- src/main/java/rba_demo_1/SimpleProducer.java
+++ src/main/java/rba_demo_1/SimpleProducer.java
@@ -1,9 +1,23 @@
 package rba_demo_1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class SimpleProducer {
 
 
     public static void main(String[] args) {
+
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
+
 
     }
 }

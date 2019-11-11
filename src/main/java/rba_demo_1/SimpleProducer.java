f1f6c65bd780db491ab376c732f5b51bc4463052
Igor Buzatovic <igor.buzatovic@inovatrend.com>
2019-11-11 12:26:33 +0100

    Produce some sample messages to topic

--- src/main/java/rba_demo_1/SimpleProducer.java
+++ src/main/java/rba_demo_1/SimpleProducer.java
@@ -1,15 +1,18 @@
 package rba_demo_1;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 public class SimpleProducer {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Properties config = new Properties();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -18,6 +21,18 @@ public class SimpleProducer {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(config);
 
-
+        String topic = "rba-demo-1";
+        int msgCount = 10;
+        for (int i = 0; i < msgCount; i++) {
+            String key = "key-" + RandomStringUtils.randomNumeric(3, 4);
+            String message = "Message from Java app created at: " + LocalDateTime.now();
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, message);
+            producer.send(record);
+            System.out.println("Produced message: " + message);
+            Thread.sleep(500);
+        }
+        producer.flush();
+        producer.close();
+        System.out.println("Done");
     }
 }

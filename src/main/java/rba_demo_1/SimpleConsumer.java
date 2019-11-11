c02012ad929e0d69be90edde9b391ab5ebcf78b6
Igor Buzatovic <igor.buzatovic@inovatrend.com>
2019-11-11 14:08:24 +0100

    Added ConsumerRebalanceListener

--- src/main/java/rba_demo_1/SimpleConsumer.java
+++ src/main/java/rba_demo_1/SimpleConsumer.java
@@ -1,13 +1,13 @@
 package rba_demo_1;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -21,10 +21,11 @@ public class SimpleConsumer {
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group-1");
 
+
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
 
         String topic = "rba-demo-1";
-        consumer.subscribe(Collections.singletonList(topic));
+        consumer.subscribe(Collections.singletonList(topic), new RebalanceListener());
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.of(100, ChronoUnit.MILLIS));
@@ -33,5 +34,19 @@ public class SimpleConsumer {
             }
         }
 
+    }
+
+
+    static class RebalanceListener implements ConsumerRebalanceListener {
+
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            System.out.println("Partitions revoked: " + Arrays.toString(partitions.toArray()));
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            System.out.println("Partitions assigned: " + Arrays.toString(partitions.toArray()));
+        }
     }
 }
